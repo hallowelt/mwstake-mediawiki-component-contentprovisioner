8a54235da80e40812d1a07f3e9bc5bd1a6cf660b
akulbii <alk@astarios.com>
2022-10-19 17:05:41 +0300

    Get rid of static call

--- bootstrap.php
+++ bootstrap.php
@@ -4,11 +4,11 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION', '1.0.0' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION', '1.0.2' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'contentprovisioner', static function () {
 	$GLOBALS['wgHooks']['LoadExtensionSchemaUpdates'][] =
 		// phpcs:ignore Generic.Files.LineLength.TooLong
-		'\\MWStake\\MediaWiki\\Component\\ContentProvisioner\\MediaWiki\\Hook\\LoadExtensionSchemaUpdates\\RunUpdate::onLoadExtensionSchemaUpdates';
+		'\\MWStake\\MediaWiki\\Component\\ContentProvisioner\\MediaWiki\\Hook\\LoadExtensionSchemaUpdates\\RunUpdate';
 } );
