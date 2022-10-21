f98c21c1649daa60dd77acbcb27052e2fa98fb20
akulbii <alk@astarios.com>
2022-10-21 19:21:30 +0300

    Change hook to proper static call

--- src/MediaWiki/Hook/LoadExtensionSchemaUpdates/RunUpdate.php
+++ src/MediaWiki/Hook/LoadExtensionSchemaUpdates/RunUpdate.php
@@ -2,10 +2,20 @@
 
 namespace MWStake\MediaWiki\Component\ContentProvisioner\MediaWiki\Hook\LoadExtensionSchemaUpdates;
 
+use DatabaseUpdater;
 use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
 use MWStake\MediaWiki\Component\ContentProvisioner\MediaWiki\Maintenance\ProvisionContents;
 
 class RunUpdate implements LoadExtensionSchemaUpdatesHook {
+
+	/**
+	 * @param DatabaseUpdater $updater
+	 * @return bool
+	 */
+	public static function callback( $updater ) {
+		$provider = new static();
+		return $provider->onLoadExtensionSchemaUpdates( $updater );
+	}
 
 	/**
 	 * @inheritDoc
