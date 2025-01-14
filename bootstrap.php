a38c2c03acaafc6155eddeef6d15f265448ea00f
d.savuljesku <d.savuljesku@gmail.com>
2025-01-14 13:41:57 +0100

    Remove deprecated access to

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,21 @@
 <?php
 
+use MWStake\MediaWiki\Component\ContentProvisioner\MediaWiki\Hook\LoadExtensionSchemaUpdates\RunUpdate;
+
 if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION', '2.1.4' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION', '2.1.5' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'contentprovisioner', static function () {
 	$GLOBALS['mwsgContentProvisionerSkip'] = [];
 
-	$GLOBALS['wgHooks']['LoadExtensionSchemaUpdates'][] =
-		'\\MWStake\\MediaWiki\\Component\\ContentProvisioner\\'
-		. 'MediaWiki\\Hook\\LoadExtensionSchemaUpdates\\RunUpdate::callback';
+	$GLOBALS['wgExtensionFunctions'][] = static function() {
+		$hookContainer = \MediaWiki\MediaWikiServices::getInstance()->getHookContainer();
+		$hookContainer->register( 'LoadExtensionSchemaUpdates', [ RunUpdate::class, 'callback' ] );
+	};
 
-	$GLOBALS['wgServiceWiringFiles'][] = $GLOBALS['IP']
-		. '/vendor/mwstake/mediawiki-component-contentprovisioner/includes/ServiceWiring.php';
+	$GLOBALS['wgServiceWiringFiles'][] = __DIR__ . '/includes/ServiceWiring.php';
 } );
