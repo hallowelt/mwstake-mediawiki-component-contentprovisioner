86aace0d240c4e35361cf5852d3f6187aa7340dc
hslater <hamish.slater@hallowelt.com>
2025-07-10 16:11:17 +1200

    Skip content provisioning during CI
    ERM42730

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION', '3.0.1' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_CONTENTPROVISIONER_VERSION', '3.0.2' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'contentprovisioner', static function () {
